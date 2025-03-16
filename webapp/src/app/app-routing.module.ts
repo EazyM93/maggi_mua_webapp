426e9c866abe16e3345ae4b1915e829a9cb0cc4e
Manuel Centini Pro <centini.manuel.93@gmail.com>
2025-03-16 14:22:51 +0100

    refctor

--- webapp/src/app/app-routing.module.ts
+++ webapp/src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
-import { TruccoSposaComponent } from './components/trucco-sposa/trucco-sposa.component';
+import { ContattiComponent } from './components/contatti/contatti.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'trucco-sposa', component: TruccoSposaComponent }
+  { path: 'contatti', component: ContattiComponent }
 ];
 
 @NgModule({
--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -4,17 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { TruccoSposaComponent } from './components/trucco-sposa/trucco-sposa.component';
 
 import { GalleriaModule } from 'primeng/galleria';
+import { ContattiComponent } from './components/contatti/contatti.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavbarComponent,
-    TruccoSposaComponent
+    ContattiComponent
   ],
   imports: [
     BrowserModule,
