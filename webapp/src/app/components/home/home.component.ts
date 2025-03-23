40b51818d00d7c9ef4b5804270648fe98bd8c447
Manuel Centini Pro <centini.manuel.93@gmail.com>
2025-03-23 16:55:14 +0100

    up

--- webapp/src/app/components/home/home.component.ts
+++ webapp/src/app/components/home/home.component.ts
@@ -1,5 +1,4 @@
 import { Component, inject, model, OnInit } from '@angular/core';
-import { PortfolioService } from '../../services/portfolio.service';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +6,9 @@ import { PortfolioService } from '../../services/portfolio.service';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent implements OnInit{
+
+  testoMain = `<b>Pro make up artist</b> con sede a <b>Roma</b>.<br><br>La mia passione per il trucco è nata grazie alla mia esperienza nel settore del <b>Beauty Retail</b>. Per affinare ulteriormente le mie competenze, ho seguito un <b>Corso Professionale</b> che mi ha permesso di esplorare le tecniche più avanzate e le ultime tendenze del settore. Ho avuto il privilegio di lavorare presso <b>set televisivi e sfilate</b>, realizzando look sofisticati e personalizzati per ogni occasione.<br><br>Il mio stile è <b>elegante, glam e pulito</b> grazie all'ausilio di prodotti <b>Luxury</b> che garantiscono un'eccellente <b>Experience e Tenuta</b>.`;
+
 
   testoSposa = 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo. Nemo enim ipsam voluptatem, quia voluptas sit, aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos, qui ratione voluptatem sequi nesciunt, neque porro quisquam est, qui dolorem ipsum, quia dolor sit, amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt, ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit, qui in ea voluptate velit esse, quam nihil molestiae consequatur, vel illum, qui dolorem eum fugiat, quo voluptas nulla pariatur.';
 
